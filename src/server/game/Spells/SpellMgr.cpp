7a9d3bc3540be5f5c82ff7f408e196d5c0433305
shadoney <darkgangsta06@hotmail.com>
2012-10-07 15:17:58 +0200

    Core/Talent Grand Crusader

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3969,6 +3969,16 @@ void SpellMgr::LoadSpellCustomAttr ()
             spellInfo->procChance = 20;
             count++;
             break;
+        case 75806:          // Grand Crusader rank1
+           spellInfo->procChance = 10;
+			spellInfo->EffectTriggerSpell[0] = 85416;
+           count++;
+           break;
+       case 85043:          // Grand Crusader rank2
+           spellInfo->procChance = 20;
+			spellInfo->EffectTriggerSpell[0] = 85416;
+           count++;
+           break;
         case 2643:          // Multi-Shot no-target Effect 0 fix.
             spellInfo->EffectImplicitTargetA[0] = TARGET_DST_TARGET_ENEMY;
             count++;
